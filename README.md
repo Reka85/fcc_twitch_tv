05198345c3b7f6ac2e06f3aa19296d35372520ca
Reka Kulinyi <reekaa85@gmail.com>
2018-04-04 23:11:34 +0200

    search field added

---
+++ README.md
@@ -0,0 +1,2 @@
+TwitchTV channel search
+Web application that uses Twitch.tv API to display channels that are streaming at the moment
--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
-  const button = $("#button");
+  const button = $("#search-button");
+  const searchField = $("#search-field");
 
   button.click(function(){
-    $.getJSON("https://wind-bow.glitch.me/twitch-api/streams/food", function(data) {
-      //console.log(data)
-      if(data.stream){
-        const channelDetails = data.stream.channel
-        $("#name").append(
-          `<span>${channelDetails.display_name}</span>`
-          );
-        $("#channel-url").append(
-          `<a href="${channelDetails.url}">${channelDetails.status}</a>`
-          );
-        $("#fps").append(
-          `<span>${data.stream.average_fps}</span>`
-          );
-        $("#channel").append(
-          `<img src=\"${channelDetails.logo}\"/>`
-          );
-      } else {
-        $("#channel").append("<p>Channel is not streaming at the moment. Try again later!</p>")
-      }
-    });
+    const channelName = searchField.val();
+    if (channelName){
+      $.getJSON(`https://wind-bow.glitch.me/twitch-api/streams/${channelName}`, function(data) {
+        if(data.stream){
+          const channelDetails = data.stream.channel
+          $("#name").append(
+            `<span>${channelDetails.display_name}</span>`
+            );
+          $("#channel-url").append(
+            `<a href="${channelDetails.url}">${channelDetails.status}</a>`
+            );
+          $("#fps").append(
+            `<span>${data.stream.average_fps}</span>`
+            );
+          $("#channel").append(
+            `<img src=\"${channelDetails.logo}\"/>`
+            );
+        } else {
+          $("#channel").html('');
+          $("#channel").append("<p>This channel is not streaming at the moment. Try again later!</p>")
+        }
+      });
+    } else {
+      $("#channel").html('');
+      $("#channel").append("<p>Please enter a channel name</p>")
+    }
   });
 });
 
