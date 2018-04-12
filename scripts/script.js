0d4f280a72a7491df1c007a24ff3b54834df15e2
Reka Kulinyi <reekaa85@gmail.com>
2018-04-12 18:02:47 +0200

    card style

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,50 @@
-$(document).ready(function(){
-  const button = $("#search-button");
+document.addEventListener("DOMContentLoaded", function(event) {
+  const channelList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  const allButton = document.getElementById('all');
 
-  const channel = $("#channel");
-
-
-  button.click(function(){
-    const channelName = searchField.val();
-    if (channelName){
-      $.getJSON(`https://wind-bow.glitch.me/twitch-api/streams/${channelName}`, function(data) {
-        if(data.stream){
-          const channelDetails = data.stream.channel
-          $("#name").append(
-            `<span>${channelDetails.display_name}</span>`
-            );
-          $("#channel-url").append(
-            `<a href="${channelDetails.url}">${channelDetails.status}</a>`
-            );
-          $("#fps").append(
-            `<span>${data.stream.average_fps}</span>`
-            );
-          channel.append(
-            `<img src=\"${channelDetails.logo}\"/>`
-            );
-        } else {
-          channel.append("<p>This channel is not streaming at the moment. Try again later!</p>")
-        }
-      });
-    } else {
-      channel.append("<p>Please enter a channel name</p>")
-    }
+  allButton.addEventListener("click", function(){
+    channelSetup(channelList);
   });
+
 });
 
-// // without the arrow function
-// }).then(function(response) {
-//     return response.json();
-// })
+function channelSetup(channels){
+  channels.forEach(function(channel){
+    fetch(`https://wind-bow.glitch.me/twitch-api/channels/${channel}`)
+          .then(response => response.json())
+          .then(function(data){
+            const name = data.display_name;
+            const logo_url = data.logo_url;
+            const status = data.status;
+            const url = data.url;
+            const image = data.video_banner;
+            const followers = data.followers;
 
-// // using the arrow function
-// }).then(response => response.json())
+            createChannelHtml(name,status,url, image,followers);
+            addChannelImage(name, image);
+          })
+  });
+}
+
+function createChannelHtml(channelName, channelStatus, channelUrl, channelImage, channelFollowers){
+
+  const channelCard = `<div class="col-xs-12 col-sm-6 col-md-4">
+                        <div class="channel-card">
+                          <div class="card-image" id="${channelName}">
+                          </div>
+                          <div class="card-details">
+                            <h5 class="card-channel-name">${channelName}</h5>
+                            <p class="card-channel-followers">${channelFollowers} followers</p>
+                            <a target="_blank" href="${channelUrl}">${channelStatus.substring(0,48)}<span class="card-link"></span></a>
+                          </div>
+                        </div>
+                      </div>`
+  const channelsElem =document.querySelector("#channels .row");
+  channelsElem.insertAdjacentHTML("afterbegin", channelCard);
+}
+
+function addChannelImage(channelName, channelImage){
+  const channel = document.getElementById(`${channelName}`);
+  channel.style.backgroundImage = channelImage ? `url('${channelImage}')` : "url('./images/no-image-available.jpg')"
+}
+
